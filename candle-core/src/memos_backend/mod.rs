a93411a510785f5faf9191afb05e858a1d93675c
Daniel Bittman <danielbittman1@gmail.com>
2026-02-19 12:39:38 -0800

    Cleanup.

--- candle-core/src/memos_backend/mod.rs
+++ candle-core/src/memos_backend/mod.rs
@@ -455,7 +455,7 @@ impl MemOSBuilder {
         }
     }
 
-    pub fn write_hdr(&self, off: u64, id: ObjID) {
+    pub fn write_hdr(&mut self, off: u64, id: ObjID) {
         self.hdr.with_tx(|tx| {
             tx.base_mut().ptr = GlobalPtr::new(id, off);
             Ok(())
